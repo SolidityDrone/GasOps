32fc324e89515c52225d1b114da23aca7c14cc1c
SolidityDrone <pnn.claudio@gmail.com>
2025-06-24 00:00:24 +0200

    feat::functions_fullfill

--- contracts/test/Counter.t.sol
+++ contracts/test/Counter.t.sol
@@ -2,7 +2,13 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
+import {GasHedger} from "../src/GasHedger.sol";
 
 contract CounterTest is Test {
-   
+    GasHedger public gasHedger;
+
+    function setUp() public {
+        gasHedger = new GasHedger(0x0000000000000000000000000000000000000000, 0x0000000000000000000000000000000000000000);
+    }
+    
 }
