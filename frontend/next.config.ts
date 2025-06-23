7ab2289f25a241cf1652f749615c7d74463da982
SolidityDrone <pnn.claudio@gmail.com>
2025-06-24 01:05:16 +0200

    feat::added_frontentd_next_starter

---
+++ frontend/next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig = {
+  webpack: (config: { externals: string[]; }) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+};
+export default nextConfig;
